dc071cd4f2cdf673d92b6b2e1e2afa8c35fe7dcd
June Cho <junecho.ucb@gmail.com>
2023-01-26 10:25:32 -0800

    cleanUp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/NavBar';
+import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Banner />
 
     </div>
   );
