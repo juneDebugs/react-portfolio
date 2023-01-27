c1af6be09b64495981dc4f3061d037b20ca02cd2
June Cho <junecho.ucb@gmail.com>
2023-01-26 17:52:34 -0800

    cleanUp

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,19 +39,19 @@ export const Skills = () => {
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Public Speaking</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Video Content</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Technical Blogs</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Team Leadership</h5>
                             </div>
                         </Carousel>
                     </div>
