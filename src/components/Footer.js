a695406d3fcaa3c7fe1a898c91e8ed9616da58ab
June Cho <junecho.ucb@gmail.com>
2023-02-03 09:58:03 -0800

    update links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,8 +17,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/chojune/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://twitter.com/juneDebugs"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>June Cho. Copyright 2023. All Rights Reserved</p>
