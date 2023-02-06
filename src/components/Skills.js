eadc24ef8cdda22f604c055514b39d4ec0a3823b
June Cho <junecho.ucb@gmail.com>
2023-02-06 15:45:27 -0800

    cleanUp Lorem

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -36,7 +36,7 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Past Appearances</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>"If you want to master something, teach it.”<br></br>I find inspiration through conversations with individuals from the design and developer community. </p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
